3398b5f2b1947cede739953f75735ab86d5714b8
XCore0 <ihsanhadinugroho.8h@gmail.com>
2026-09-20 15:27:50 +0700

    up migrate

---
+++ database/migrations/2026_09_20_000005_create_anggota_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('anggota', function (Blueprint $table) {
+            $table->id('id_anggota');
+            $table->string('id_pengguna', 50);
+            $table->string('status_keanggotaan', 50)->default('Aktif'); // Aktif, Tidak Aktif
+            $table->timestamps();
+
+            $table->foreign('id_pengguna')->references('id_pengguna')->on('pengguna')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('anggota');
+    }
+};
